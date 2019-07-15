55913cf289ec3ec36417c72391b821d4b2a633b0
zc <416938465@qq.com>
2019-07-15 17:49:10 +0800

    complete list sort

--- pages/sort/sort.js
+++ pages/sort/sort.js
@@ -7,6 +7,8 @@ Page({
   data: {
     teachers: [],
     isSwitch: false,
+    isTeachersMsgView: false,
+    teacherViewMsg: null,
     notice: "各位同学请注意：导师顺序选择对最终导师分配结果有较大影响，请认真选择"
   },
 
@@ -115,13 +117,51 @@ Page({
   changePosition: function(e){
     // console.log(e.currentTarget.dataset.index);
     // console.log(e.target.id);
+
     // 获取当前教师卡index
     let index = e.currentTarget.dataset.index;
-    
+    let teachers = this.data.teachers;
     if(e.target.id=="upper"){
-      console.log("upper "+index);
+      if(index==0)
+        return ;
+      else{
+        let temp = teachers[index];
+        teachers[index] = teachers[index - 1];
+        teachers[index - 1] = temp;
+        this.setData({
+          teachers: teachers
+        })
+      }
     } else if (e.target.id == "down") {
-      console.log("down "+index);
+      if(index==4)
+        return ;
+      else{
+        let temp = teachers[index];
+        teachers[index] = teachers[index + 1];
+        teachers[index + 1] = temp;
+        this.setData({
+          teachers: teachers
+        })
+      }
+    }
+  },
+  onView: function(e){
+    // console.log(e.target.id);
+    // console.log(e.target.dataset.index);
+    if(e.target.id!="upper" && e.target.id!="down" && e.target.id!="upperAndDown"){
+      let teacherMsg = this.data.teachers[e.target.dataset.index];
+      this.setData({
+        isTeachersMsgView: true,
+        teacherViewMsg: teacherMsg
+      })
+      // console.log(this.data.teacherViewMsg);
+    }
+  },
+  offView: function(e){
+    if(e.target.id=="off"){
+      this.setData({
+        isTeachersMsgView: false
+      })
     }
   }
 })
